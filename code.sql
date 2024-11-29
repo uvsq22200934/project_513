1c2f24c4ae9cbb83977213408b0fa2225aa46bb9
uvsq22200379 <114011966+uvsq22200379@users.noreply.github.com>
2024-11-29 19:14:15 +0100

    correction des erreurs pour la création des tables

--- code.sql
+++ code.sql
@@ -1,140 +1,131 @@
 --CREATION DE TABLE
 
--- Table FILMSERIE
+CREATE TABLE SERIE (
+                       id_serie INT PRIMARY KEY ,
+                       nom_serie VARCHAR (25) NOT NULL
+);
 CREATE TABLE FILMSERIE (
-    id_film INT,
-    type VARCHAR(50) CHECK (type IN ('film', 'série')),
-    titre VARCHAR(255) NOT NULL,
-    num_saison INT,
-    num_episode INT,
-    duree INT NOT NULL, -- durée en minutes
-    date_sortie DATE,
-    pictogramme VARCHAR(255) CHECK (pictogramme IN('mal-voyant', '-12', '-16', '-18', 'contenu explicite', 'violence', 'sexe', 'Tous public')),
-    origine VARCHAR(255) CHECK (origine IN('Histoire vraie', 'fait divers', 'adaptation cinématographique')),
-    PRIMARY KEY (id_film)
+                           id_film INT,
+                           id_serie INT,
+                           type VARCHAR(10) CHECK (type IN ('Film', 'Serie')),
+                           titre VARCHAR(100) NOT NULL,
+                           num_saison INT,
+                           num_episode INT,
+                           duree INT NOT NULL,
+                           date_sortie DATE,
+                           pictogramme VARCHAR(25) CHECK (pictogramme IN('mal-voyant', '-12', '-16', '-18', 'contenu explicite', 'violence', 'sexe', 'Tous public')),
+                           origine VARCHAR(25) CHECK (origine IN('Histoire vraie', 'Fait divers', 'Adaptation cinématographique')),
+                           PRIMARY KEY (id_film)
 );
-
--- Table CATEGORIE
 CREATE TABLE CATEGORIE (
-    id_categorie INT PRIMARY KEY,
-    genre VARCHAR(255) NOT NULL
+                           id_categorie INT PRIMARY KEY,
+                           genre VARCHAR(25) NOT NULL
 );
-
--- Table CLASSER
 CREATE TABLE CLASSER (
-    id_categorie INT,
-    id_film INT,
-    FOREIGN KEY (id_categorie) REFERENCES CATEGORIE(id_categorie),
-    FOREIGN KEY (id_film) REFERENCES FILMSERIE(id_film)
+                         id_categorie INT,
+                         id_film INT,
+                         FOREIGN KEY (id_categorie) REFERENCES CATEGORIE(id_categorie),
+                         FOREIGN KEY (id_film) REFERENCES FILMSERIE(id_film)
 );
-
--- Table PARENT
 CREATE TABLE PARENT (
-    id_film1 INT, -- Premier film/série dans la relation
-    id_film2 INT, -- Second film/série dans la relation
-    UNIQUE (id_film1, id_film2), -- Contrainte pour éviter les doublons
-    FOREIGN KEY (id_film1) REFERENCES FILMSERIE(id_film), -- Référence au film/série 1
-    FOREIGN KEY (id_film2) REFERENCES FILMSERIE(id_film) -- Référence au film/série 2
+                        id_film1 INT, -- Premier film/série dans la relation
+                        id_film2 INT, -- Second film/série dans la relation
+                        UNIQUE (id_film1, id_film2), -- Contrainte pour éviter les doublons
+                        FOREIGN KEY (id_film1) REFERENCES FILMSERIE(id_film), -- Référence au film/série 1
+                        FOREIGN KEY (id_film2) REFERENCES FILMSERIE(id_film) -- Référence au film/série 2
 );
-
-
--- Table LANGUE
 CREATE TABLE LANGUE (
-    id_langue INT PRIMARY KEY,
-    code VARCHAR(10) NOT NULL UNIQUE, -- Ex : FR, EN, ES
-    nom_langue VARCHAR(255) NOT NULL -- Ex : Français, Anglais, Espagnol
+                        id_langue INT PRIMARY KEY,
+                        code VARCHAR(10) NOT NULL UNIQUE, -- Ex : FR, EN, ES
+                        nom_langue VARCHAR(30) NOT NULL -- Ex : Français, Anglais, Espagnol
 );
--- Table DISPONIBLE
+
 CREATE TABLE DISPONIBLE (
-    id_film INT, -- Film ou série concerné(e)
-    langue_audio VARCHAR(10) NOT NULL,  -- Code de langue pour l'audio (ex : FR, EN, etc.)
-    langue_sous_titre VARCHAR(10), -- Code de langue pour les sous-titres
-    PRIMARY KEY (id_film, langue_audio, langue_sous_titre), -- Unicité des langues par film
-    FOREIGN KEY (id_film) REFERENCES FILMSERIE(id_film) -- Référence au film
+                            id_langue INT,
+                            id_film INT, -- Film ou série concerné(e)
+                            langue_audio VARCHAR(10) NOT NULL,  -- Code de langue pour l'audio (ex : FR, EN, etc.)
+                            langue_sous_titre VARCHAR(10), -- Code de langue pour les sous-titres
+                            PRIMARY KEY (id_film, langue_audio, langue_sous_titre), -- Unicité des langues par film
+                            FOREIGN KEY (id_film) REFERENCES FILMSERIE(id_film) -- Référence au film
 );
 
--- Table SPECTATEUR
 CREATE TABLE SPECTATEUR (
-    id_spectateur INT PRIMARY KEY,
-    nom VARCHAR(255),
-    prenom VARCHAR(255),
-    age INT CHECK (age >= 16)
-    sexe VARCHAR(50) CHECK (sexe IN ('Homme', 'Femme')),
+                            id_spectateur INT PRIMARY KEY,
+                            nom VARCHAR(30),
+                            prenom VARCHAR(30),
+                            age INT CHECK (age >= 16),
+                            sexe VARCHAR(15) CHECK (sexe IN ('Homme', 'Femme'))
 );
 
--- Table CRITIQUE
 CREATE TABLE CRITIQUE (
-    id_critique INT PRIMARY KEY,
-    id_film INT,
-    id_spectateur INT,
-    note INT CHECK (note BETWEEN 0 AND 10),
-    commentaire TEXT,
-    FOREIGN KEY (id_film) REFERENCES FILMSERIE(id_film),
-    FOREIGN KEY (id_spectateur) REFERENCES SPECTATEUR(id_spectateur)
+                          id_critique INT PRIMARY KEY,
+                          id_film INT,
+                          id_spectateur INT,
+                          note INT CHECK (note BETWEEN 0 AND 10),
+                          commentaire VARCHAR(255),
+                          FOREIGN KEY (id_film) REFERENCES FILMSERIE(id_film),
+                          FOREIGN KEY (id_spectateur) REFERENCES SPECTATEUR(id_spectateur)
 );
 
--- Table PLATEFORME
 CREATE TABLE PLATEFORME (
-    id_plateforme INT PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL
+                            id_plateforme INT PRIMARY KEY,
+                            nom VARCHAR(30) NOT NULL
 );
 
--- Table DIFFUSE
 CREATE TABLE DIFFUSE (
-    id_film INT,
-    id_plateforme INT,
-    date_dispo DATE,
-    duree_dispo INT CHECK (duree_dispo > 0), -- durée en jours
-    PRIMARY KEY (id_film, id_plateforme),
-    FOREIGN KEY (id_film) REFERENCES FILMSERIE(id_film),
-    FOREIGN KEY (id_plateforme) REFERENCES PLATEFORME(id_plateforme)
+                         id_film INT,
+                         id_plateforme INT,
+                         date_dispo DATE,
+                         duree_dispo INT CHECK (duree_dispo > 0), -- durée en jours
+                         PRIMARY KEY (id_film, id_plateforme),
+                         FOREIGN KEY (id_film) REFERENCES FILMSERIE(id_film),
+                         FOREIGN KEY (id_plateforme) REFERENCES PLATEFORME(id_plateforme)
 );
-
--- Table VISIONNE
 CREATE TABLE VISIONNE (
-    id_visionnage INT PRIMARY KEY,
-    id_film INT,
-    id_plateforme INT,
-    date_visionnage DATE,
-    temps_visionnage INT, -- temps en minutes
-    temps_pause INT, -- temps de pause en minutes
-    FOREIGN KEY (id_film) REFERENCES FILMSERIE(id_film),
-    FOREIGN KEY (id_plateforme) REFERENCES PLATEFORME(id_plateforme)
+                          id_visionnage INT PRIMARY KEY,
+                          id_film INT,
+                          id_plateforme INT,
+                          date_visionnage DATE,
+                          temps_visionnage INT, -- temps en minutes
+                          temps_pause INT,-- temps de pause en minutes
+                          langue_audio VARCHAR (15),
+                          langue_sous_titre VARCHAR(15),
+                          FOREIGN KEY (id_film) REFERENCES FILMSERIE(id_film),
+                          FOREIGN KEY (id_plateforme) REFERENCES PLATEFORME(id_plateforme)
 );
 
--- Table ABONNE
 CREATE TABLE ABONNE (
-    id_plateforme INT,
-    id_spectateur INT,
-    date_abo DATE,
-    prix_abo DECIMAL(5, 2) CHECK (prix_abo >= 0),
-    PRIMARY KEY (id_plateforme, id_spectateur),
-    FOREIGN KEY (id_plateforme) REFERENCES PLATEFORME(id_plateforme),
-    FOREIGN KEY (id_spectateur) REFERENCES SPECTATEUR(id_spectateur)
+                        id_plateforme INT,
+                        id_spectateur INT,
+                        date_abo DATE,
+                        prix_abo DECIMAL(5, 2) CHECK (prix_abo >= 0),
+                        duree_abo INT CHECK( duree_abo > 0), --temps en mois
+                        PRIMARY KEY (id_plateforme, id_spectateur),
+                        FOREIGN KEY (id_plateforme) REFERENCES PLATEFORME(id_plateforme),
+                        FOREIGN KEY (id_spectateur) REFERENCES SPECTATEUR(id_spectateur)
 );
 
--- Table PERSONNE
 CREATE TABLE PERSONNE (
-    nom VARCHAR(255),
-    prenom VARCHAR(255),
-    sexe VARCHAR(50),
-    age INT,
-    metier VARCHAR(255),
-    PRIMARY KEY (nom, prenom)
+                          nom VARCHAR(255),
+                          prenom VARCHAR(255),
+                          sexe VARCHAR(50),
+                          age INT,
+                          metier VARCHAR(255),
+                          PRIMARY KEY (nom, prenom)
 );
 
--- Table TRAVAILLE
 CREATE TABLE TRAVAILLE (
-    id_travaille INT PRIMARY KEY,
-    nom VARCHAR(255),
-    prenom VARCHAR(255),
-    id_film INT,
-    date_contrat_debut DATE,
-    date_contrat_fin DATE,
-    salaire DECIMAL(10, 2),
-    FOREIGN KEY (nom, prenom) REFERENCES PERSONNE(nom, prenom),
-    FOREIGN KEY (id_film) REFERENCES FILMSERIE(id_film)
+                           id_travaille INT PRIMARY KEY,
+                           nom VARCHAR(255),
+                           prenom VARCHAR(255),
+                           id_film INT,
+                           date_contrat_debut DATE,
+                           date_contrat_fin DATE,
+                           salaire DECIMAL(10, 2),
+                           FOREIGN KEY (nom, prenom) REFERENCES PERSONNE(nom, prenom),
+                           FOREIGN KEY (id_film) REFERENCES FILMSERIE(id_film)
 );
+
 
 --CONTRAINTE
 
